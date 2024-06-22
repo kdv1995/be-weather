fb408d50d182687cfb6c9ea30f8d8a756605c96b
kdv1995 <tribeofdanel@gmail.com>
2024-06-22 13:05:03 +0300

    updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WeatherModule } from './weather/weather.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     WeatherModule,
-    MongooseModule.forRoot('mongodb://localhost/weather'),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('DATABASE_URL'),
+      }),
+      inject: [ConfigService],
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: ['.env.development.local', 'env.development', '.env'],
